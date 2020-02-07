21ebe0c8c64ca014b7a86c52bf2c117551df76a8
Eric-He-98 <eric@rocketmiles.com>
2020-02-07 15:44:38 -0500

    fix script

--- train_word2vec.py
+++ train_word2vec.py
@@ -7,13 +7,10 @@ import sys
 warnings.filterwarnings(action = 'ignore') 
 
 # accept filenames as arguments
-pageviews_file = sys.argv[1]
-census_file = sys.argv[2]
-content_file = sys.argv[3]
+# only the content file is required
+content_file = sys.argv[1]
 
-print('Reading data')
-pageview = pd.read_csv(pageviews_file, sep=',', error_bad_lines=False)
-census = pd.read_csv(census_file, sep=',', error_bad_lines=False)
+print('Reading data from file {}'.format(content_file))
 content = pd.read_csv(content_file, sep=',', error_bad_lines=False)
 
 # select features from original dataset
@@ -29,12 +26,21 @@ print('Training model')
 word2vec = Word2Vec(content['merged_words'], min_count=3, size=32, workers=3, window=3, sg=0) # CBOW
 
 # extract embeddings
-print('Extracting embeddings')
+print('Extracting word embeddings')
 word_list = list(word2vec.wv.vocab.keys())
 word_embeddings = pd.Series(word_list, index=word_list).apply(lambda x: word2vec[x])
 df = pd.DataFrame.from_dict(word_embeddings.to_dict()).T
 colnames = ['embed_' + str(i) for i in range(32)]
 df.columns = colnames
 
-print('Writing embeddings to csv')
-df.to_csv('data/w2v_df.csv')
+# extract content embeddings
+print('Computing content embeddings')
+content_embeddings = content.apply(lambda row: df.loc[row['merged_words']].mean(axis=0), axis=1)
+content_embeddings = content_embeddings.set_index(content['url'])
+
+words_output_file = 'data/w2v_df.csv'
+content_output_file = 'data/content_embeddings.csv'
+print('Writing word embeddings to csv at location {}'.format(words_output_file))
+df.to_csv(words_output_file)
+print('Writing content embeddings to csv at location {}'.format(content_output_file))
+content_embeddings.to_csv(content_output_file) 
